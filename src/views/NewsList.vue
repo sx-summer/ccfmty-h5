76ce1de5b71e2deab7b3e1bdb7ab412775eb9d3b
ziyi <ziyi@meili-inc.com>
2021-04-12 17:17:30 +0800

    feat:添加新闻列表

--- src/views/NewsList.vue
+++ src/views/NewsList.vue
@@ -1,41 +1,70 @@
 <template>
-  <div class="home">
+  <div class="newsList">
     <HeadBar title="新闻列表" />
-    <div class="box">
-      <van-button type="info" @click="onSubmit">新闻列表</van-button>
-    </div>
+    <h3 class="newsTitleH3">新闻列表</h3>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="(item,index) in newsData" :key="item.id" :title="`${index+1}、${item.newsName}`" @click="goDetailLink(item)" />
+    </van-list>
   </div>
 </template>
 
 <script>
   import HeadBar from "@/components/HeadBar.vue";
+  import { getQueryString, fetchHttp, formatTime } from "@/util/fn.js";
+
   import {
     Toast,
-    Button
+    Button,
+    List,
+    Cell
   } from "vant";
 
   export default {
     name: "home",
     components: {
       HeadBar,
-      [Button.name]: Button
+      [Button.name]: Button,
+      [List.name]: List,
+      [Cell.name]: Cell
     },
     data() {
       return {
+        newsData: [],
+        loading: false,
+        finished: false,
+        currentPage: 1,
+        totalPage: 1,
       };
     },
     computed: {
     },
     methods: {
-      onSubmit() {
-        Toast("新闻列表");
+      onLoad() {
+        fetchHttp('marathon/newsInfoListPage', { page: this.currentPage, size: 1000 }, 'GET').then(res => {
+          if (res && res.code === 0) {
+            if (res.data && res.data.list) {
+              this.newsData = res.data.list
+              this.loading = false;
+              this.finished = true;
+            } else {
+              Toast('没有找到相关新闻');
+            }
+          }
+        });
+      },
+      goDetailLink(item) {
+        window.location.href = 'newsDetail?newsId=' + item.id;
       }
     }
   };
 </script>
 <style scope lang="less">
-  .home {
-    text-align: center;
+  .newsList {
+    color: #333333;
+    font-size: 16px;
   }
-
+  .newsTitleH3{
+    border-bottom: 2px solid #ddd;
+    padding: 0 0px 5px 15px;
+  }
 </style>
